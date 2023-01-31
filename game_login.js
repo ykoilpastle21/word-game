3d1b68fff36841fe5ab0fe40b7a4725946fa7a7b
ykoilpastle21 <107996692+ykoilpastle21@users.noreply.github.com>
2023-01-31 19:33:56 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function adduser() {
+    player1_name = document.getElementById("player1_id").value;
+    player2_name = document.getElementById("player2_id").value;
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+    window.location = "game_page.html"
+}
